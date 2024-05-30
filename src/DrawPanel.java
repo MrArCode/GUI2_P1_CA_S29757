59cf799b8c5de6fd6d81e0b15a5bb300b8f495e2
MrArCode <a.c.posluszny@gmail.com>
2024-05-30 13:54:57 +0200

    Before adding erase figure option

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -79,7 +79,6 @@ public class DrawPanel extends JPanel {
         });
     }
 
-    @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
